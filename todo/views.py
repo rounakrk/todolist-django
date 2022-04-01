da2ff5c862b68e816a32a215fc3c9c5e45d92139
rounakrk <rounakagarwal64@gmail.com>
2022-04-01 17:44:36 +0530

    Completed the functionality
    Signed-off-by: rounakrk <rounakagarwal64@gmail.com>

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TODO
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    todo = TODO.objects.all()
+    if request.method == 'POST':
+        task_name = request.POST.get('task')
+        new_todo = TODO(task_name=task_name)
+        new_todo.save()
+        return redirect('/')
+
+    return render(request, 'index.html', {'todos':todo})
+
+def delete(request, pk):
+    todo = TODO.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
